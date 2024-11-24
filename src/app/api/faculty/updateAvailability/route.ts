10172ec5122f8e104edb2fd32d34d314838808f6
jainil prajapati <jainilprajapati8095@gmail.com>
2024-11-24 18:53:00 +0530

    Update faculty availability handling and improve sidebar component ref types

--- src/app/api/faculty/updateAvailability/route.ts
+++ src/app/api/faculty/updateAvailability/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: Request) {
     const existingFaculty = await Faculty.findOne({ mac: macid })
 
     existingFaculty.availability = status;
-  
+    await existingFaculty.save();
     return NextResponse.json({ message: 'Faculty availability changes successfully' }, { status: 201 })
   } catch (error) {
     console.error('Error change faculty availability:', error)
