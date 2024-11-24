0676d87c7b7502a9fd21ba2c11cb34447a704a34
jainil prajapati <jainilprajapati8095@gmail.com>
2024-11-24 18:34:51 +0530

    Add API endpoint to check faculty availability

---
+++ src/app/api/faculty/checkAvailability/route.tsx
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server'
+import dbConnect from '@/lib/mongodb'
+import Faculty from '@/models/Faculty'
+
+export async function POST(request: Request) {
+  try {
+    await dbConnect()
+    const {macid} = await request.json()
+
+    const faculty = await Faculty.findOne({ mac: macid })
+
+    if (!faculty) {
+      return NextResponse.json({ status: "error", data:{ message: 'Faculty not found'} }, { status: 404 })
+    }
+  
+    let isAvailable = faculty.availability
+    let message = `faculty is ${isAvailable ? 'available' : 'not available'}`
+
+    return NextResponse.json({ status: "success", data: { isAvailable: faculty.availability, message:message } }, { status: 201 })
+  } catch (error) {
+    console.error('Error change faculty availability:', error)
+    return NextResponse.json({ error: 'Failed to changes faculty availability' }, { status: 500 })
+  }
+}
+
